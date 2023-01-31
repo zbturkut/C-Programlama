1c04ef42519ac5d54c21c7749662e93e7ef1e2cb
zbturkut <zbturkut@gmail.com>
2023-01-31 15:48:28 +0300

    Veri Tipleri

---
+++ toplama-cikarma.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+
+	print("%d\n",12+4);
+	
+	
+	
+	
+	
+	
+	
+	
+  return 0 ;
+}
---
+++ veri-tipleri.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+	
+  int a1 = 4;
+  char b1='b';
+  float c1= 1.72;
+  double d1= 0.22;
+  printf("%d %c %f %.1f\n" ,a1,b1,c1,d1 ,"Veri tipleri" );
+  
+ getch();
+ return 0;
+}
